d2d402d18dddc43da85c0e30b0b90f9067de8c82
Elmer Torres <elmertalucod3@gmail.com>
2020-09-05 22:17:27 +0800

    add webhoooks github

--- resources/views/livewire/front-end/button.blade.php
+++ resources/views/livewire/front-end/button.blade.php
@@ -1,7 +1,7 @@
 <div>
   <button class="btn btn-primary btn-lg btn-flat" wire:click="add" wire:loading.attr="disabled">
     <span class="fas fa-plus fa-lg mr-2"></span> 
-    Notification with tone 
+    Notification with sounds
     <span class="fas fa-spinner fa-spin fa-lg mr-2" wire:loading></span> 
   </button>
 
