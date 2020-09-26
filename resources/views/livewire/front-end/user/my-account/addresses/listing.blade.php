d6f8203bd67c01905582ab969a122107faa2e53d
Christian De Leon <cmdl.deleon02@gmail.com>
2020-09-27 04:49:51 +0800

    Created the addresses layout in buyer/user @ SYG-21

--- resources/views/livewire/front-end/user/my-account/addresses/listing.blade.php
+++ resources/views/livewire/front-end/user/my-account/addresses/listing.blade.php
@@ -1,33 +1,34 @@
 <div>
 
-    <div class="row mt-2">
-        <div class="col-lg-12 px-5">
+    <div class="row">
+        <div class="col-lg-12">
             @for($x=1; $x <= 4;$x++)
-                <div class="row">
-                    <div class="col-12">
-                        <span class="fas fa-user"></span> <strong>Christian De Leon @if($x == 1)<span class="badge badge-info">Default</span>@endif</strong>
-                        <button type="button" class="btn btn-sm btn-danger float-right ml-1" title="Delete"><i class="fas fa-trash"></i></button>
-                        <button type="button" class="btn btn-sm btn-primary float-right" title="Edit"><i class="fas fa-pen"></i></button>
-                    </div>
-                </div>
-                <div>
-                    <span class="fas fa-phone"></span> +63913478864
-                </div>
-                <div class="row">
-                    <div class="col-sm-8">
-                        <span class="fas fa-map-marker"></span>
-                        Unit 301, WackWack Royal Mansion, 
-                        WackWack Greenhil, Mandaluyong City, Metro Manila
-                        Wack-Wack Greenhills, Mandaluyong City
-                        Metro Manila, Metro Manila 1555
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="text-right">
-                            <button type="button" class="btn btn-sm btn-default" @if($x == 1) disabled @endif>Set as Default</button>
+                <blockquote class="@if($x == 1) quote-primary @else quote-secondary @endif">
+                    <div class="row">
+                        <div class="col-12">
+                            <span class="fas fa-user"></span> <strong>Christian De Leon @if($x == 1)<span class="badge badge-info">Default</span>@endif</strong>
+                            <button type="button" class="btn btn-sm btn-danger float-right ml-1" title="Delete"><i class="fas fa-trash"></i></button>
+                            <button type="button" class="btn btn-sm btn-primary float-right" title="Edit"><i class="fas fa-pen"></i></button>
                         </div>
                     </div>
-                </div>
-                <hr>
+                    <div>
+                        <span class="fas fa-phone"></span> +63913478864
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <span class="fas fa-map-marker"></span>
+                            Unit 301, WackWack Royal Mansion, 
+                            WackWack Greenhil, Mandaluyong City, Metro Manila
+                            Wack-Wack Greenhills, Mandaluyong City
+                            Metro Manila, Metro Manila 1555
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="text-right">
+                                <button type="button" class="btn btn-sm btn-default" @if($x == 1) disabled @endif>Set as Default</button>
+                            </div>
+                        </div>
+                    </div>
+                </blockquote>
             @endfor
         </div>
     </div>
