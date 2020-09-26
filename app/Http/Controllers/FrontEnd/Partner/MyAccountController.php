035b2a04cdaaed0da68831d8d3dc896495f15561
Christian De Leon <cmdl.deleon02@gmail.com>
2020-09-27 03:43:55 +0800

    Created the partner/merchant profile @ SYG-14

--- app/Http/Controllers/FrontEnd/Partner/MyAccountController.php
+++ app/Http/Controllers/FrontEnd/Partner/MyAccountController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\FrontEnd\Partner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Utility;
 
 class MyAccountController extends Controller
 {
     public function index(){
-        return view('front-end.partner.my-account.index');
+        $partner = Utility::auth_partner();
+        $account = Utility::auth_user_account();
+
+        return view('front-end.partner.my-account.index', compact('partner', 'account'));
     }
 }
