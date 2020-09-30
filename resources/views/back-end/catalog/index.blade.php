5328feba017563e89b6ec2bf28210b10b10133c1
Elmer Torres <elmertalucod3@gmail.com>
2020-10-01 00:01:19 +0800

    re layour the category design in admin

--- resources/views/back-end/catalog/index.blade.php
+++ resources/views/back-end/catalog/index.blade.php
@@ -47,7 +47,7 @@
                     <input type="search" class="form-control" placeholder="Search Category Name...">
                 </div>
                 @for($x=0;$x < 10; $x++)
-                <div class="col-md-6">
+                <div class="col-12">
                     <div class="card card-warning collapsed-card">
                         <div class="card-header">
                             <h3 class="card-title">Category name</h3>
@@ -60,7 +60,25 @@
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
-                            <div class="col-12">wfw</div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <label>Add Subcategory</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Subcategory Name">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-warning">
+                                                <span class="fas fa-plus"></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                @for($i=0;$i < 10; $i++)
+                                    <div class="col-lg-4 col-md-6"><span class="fas fa-chevron-right"></span> Subcategory</div>
+                                @endfor
+                            </div>
                         </div>
                         <!-- /.card-body -->
                     </div>
