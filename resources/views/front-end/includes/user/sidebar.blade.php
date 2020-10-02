76a334b155c2d11a1ca75d3047b2762418adba81
Elmer Torres <elmertalucod3@gmail.com>
2020-10-03 02:38:54 +0800

    done buyer profile

--- resources/views/front-end/includes/user/sidebar.blade.php
+++ resources/views/front-end/includes/user/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="list-group panel">
     <a href="#account" class="list-group-item {{\Route::is('front-end.user.my-account.*') ? 'active':''}}" data-toggle="collapse" data-parent="#MainMenu">
         <span class="nav-icon fas fa-user"></span> My Account 
-        <i class="fa fa-caret-down"></i>
+        <i class="fa fa-caret-left float-right"></i>
     </a>
     
-    <div class="collapse {{\Route::is('front-end.user.my-account.*') ? 'show':''}}" id="account">
+    <div class="collapse {{\Route::is('front-end.user.my-account.*') ? 'show':''}} mb-1" id="account">
         <a href="{{route('front-end.user.my-account.index')}}" class="list-group-item {{\Route::is('front-end.user.my-account.index') ? 'sayang-link-active':''}}"><span class="fas fa-chevron-right mr-1 ml-2"></span> Profile </a>
         <a href="javascript:void(0);" class="list-group-item"><span class="fas fa-chevron-right mr-1 ml-2"></span> Banks & Cards </a>
         <a href="{{route('front-end.user.my-account.addresses')}}" class="list-group-item {{\Route::is('front-end.user.my-account.addresses') ? 'sayang-link-active':''}}""><span class="fas fa-chevron-right mr-1 ml-2"></span> Addresses </a>
@@ -13,10 +13,10 @@
 
     <a href="#dashboard" class="list-group-item" data-toggle="collapse" data-parent="#MainMenu">
         <span class="nav-icon fas fa-shopping-bag"></span> My Purchase 
-        <i class="fa fa-caret-down"></i>
+        <i class="fa fa-caret-left float-right"></i>
     </a>
 
-    <div class="collapse" id="dashboard">
+    <div class="collapse mb-1" id="dashboard">
         <a href="" class="list-group-item"><span class="fas fa-chevron-right mr-1 ml-2"></span> To Pay </a>
         <a href="" class="list-group-item"><span class="fas fa-chevron-right mr-1 ml-2"></span> Completed </a>
         <a href="" class="list-group-item"><span class="fas fa-chevron-right mr-1 ml-2"></span> Cancelled </a>
@@ -24,10 +24,10 @@
 
     <a href="#notification" class="list-group-item" data-toggle="collapse" data-parent="#MainMenu">
         <span class="nav-icon fas fa-bell"></span> Notifications 
-        <i class="fa fa-caret-down"></i>
+        <i class="fa fa-caret-left float-right"></i>
     </a>
 
-    <div class="collapse" id="notification">
+    <div class="collapse mb-1" id="notification">
         <a href="" class="list-group-item">
             <span class="fas fa-chevron-right mr-1 ml-2"></span> Order Updates <span class="badge badge-warning text-white right">6</span>
         </a>
--- resources/views/front-end/user/my-account/index.blade.php
+++ resources/views/front-end/user/my-account/index.blade.php
@@ -21,14 +21,14 @@
         </div>
     </aside> <!-- col.// -->
     <main class="col-md-9">
-        <div class="card mb-3">
+        <div class="card card-outline card-sayang mb-3">
             <div class="card-header">
                 <h5 class="card-title"> My Account</h5> 
             </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="my-account-profile mb-4">
+                        <div class="my-account-profile border-right mb-4">
                             <div class="text-center">
                                 @php
                                     if($account->photo){
@@ -39,10 +39,10 @@
                                         $photo_url = asset('images/default-photo/account.png');
                                     }
                                 @endphp
-                                <img class="profile-user-img img-fluid img-circle" src="{{$photo_url}}" alt="User profile picture">
+                                <img class="profile-user-img img-fluid img-circle m-2" style="width: 120px; height: 120px;" src="{{$photo_url}}" alt="User profile picture">
                             </div>
                             <div class="text-center mt-1">
-                                <button type="button" class="btn btn-default btn-sm">Upload Image</button>
+                                <button type="button" class="btn btn-default btn-sm">Select Profile</button>
                             </div>
                             <div class="profile-username text-center">{{ucwords($account->first_name.' '.$account->middle_name.' '.$account->last_name)}}</div>
                             <div class="text-center">
@@ -54,37 +54,80 @@
                         </div>
                     </div>
                     <div class="col-md-8">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">Username</label>
-                                <div class="col-sm-9 col-form-label">{{Auth::user()->name}}</div>
-                            </div>    
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">Email Address</label>
-                                <div class="col-sm-9 col-form-label">{{Auth::user()->email}}</div>
-                            </div>
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Username</label>
+                            <div class="col-sm-8 col-form-label">{{Auth::user()->name}}</div>
+                        </div>
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Email Address</label>
+                            <div class="col-sm-8 col-form-label">{{Auth::user()->email}}</div>
+                        </div>
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Contact Number</label>
                             @if($account->contact_no)
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Contact Number</label>
-                                    <div class="col-sm-9 col-form-label">{{Utility::mobile_number_ph_format($account->contact_no)}}</div>
+                                <div class="col-sm-8 col-form-label">{{Utility::mobile_number_ph_format($account->contact_no)}}</div>
+                            @else
+                                <div class="col-sm-8 col-form-label">
+                                    <u>
+                                        <a href=""><span class="fas fa-plus"></span> Number</a>
+                                    </u>
                                 </div>
                             @endif
-                            @if($account->gender)
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Gender</label>
-                                    <div class="col-sm-9 col-form-label">{{ucfirst($account->gender)}}</div>
+                        </div>
+                        
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Gender</label>
+                            <div class="col-sm-8 col-form-label">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" id="male" name="radio1" checked>
+                                    <label class="form-check-label" for="male">Male</label>
                                 </div>
-                            @endif
-                            @if($account->birth_date)
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label">Birth Date</label>
-                                    <div class="col-sm-9 col-form-label">{{date('F d, Y', strtotime($account->birth_date))}}</div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" id="female" name="radio1">
+                                    <label class="form-check-label" for="female">Female</label>
                                 </div>
-                            @endif
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">Member Since</label>
-                                <div class="col-sm-9 col-form-label">{{date('F Y', strtotime($account->created_at))}}</div>
                             </div>
                         </div>
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Birth Date</label>
+                            <div class="col-sm-8 col-form-label">
+                                <input type="date" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="form-group row my-0">
+                            <label class="col-sm-4 col-form-label">Member Since</label>
+                            <div class="col-sm-8 col-form-label">{{date('F d, Y', strtotime($account->created_at))}}</div>
+                        </div>
+                        {{-- <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Username</label>
+                            <div class="col-sm-8 col-form-label">{{Auth::user()->name}}</div>
+                        </div>    
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Email Address</label>
+                            <div class="col-sm-8 col-form-label">{{Auth::user()->email}}</div>
+                        </div>
+                        @if($account->contact_no)
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Contact Number</label>
+                                <div class="col-sm-8 col-form-label">{{Utility::mobile_number_ph_format($account->contact_no)}}</div>
+                            </div>
+                        @endif
+                        @if($account->gender)
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Gender</label>
+                                <div class="col-sm-8 col-form-label">{{ucfirst($account->gender)}}</div>
+                            </div>
+                        @endif
+                        @if($account->birth_date)
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Birth Date</label>
+                                <div class="col-sm-8 col-form-label">{{date('F d, Y', strtotime($account->birth_date))}}</div>
+                            </div>
+                        @endif
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Member Since</label>
+                            <div class="col-sm-8 col-form-label">{{date('F Y', strtotime($account->created_at))}}</div>
+                        </div> --}}
                     </div>
                 </div>
             </div> <!-- card-body .// -->
