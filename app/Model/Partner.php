be0ca0b53062f2ca65b64f52ef93253864fc487b
Christian De Leon <cmdl.deleon02@gmail.com>
2020-10-07 18:21:03 +0800

    Added the media model in model @ SYG-53

--- app/Model/Partner.php
+++ app/Model/Partner.php
@@ -5,10 +5,11 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\Media\Media;
 
 class Partner extends Model
 {
-    use HasHasMediaTrait;
+    use HasMediaTrait;
     
     public function user_account(){
         return $this->belongsTo('App\Model\UserAccount');
@@ -24,5 +25,9 @@ class Partner extends Model
 
     public function partner_representative(){
         return $this->hasOne('App\Model\PartnerRepresentative', 'partner_id', 'id');
+    }
+
+    public function registerMediaConversions(Media $media = null){
+        $this->addMediaConversion('medium')->height(150)->width(150);
     }
 }
