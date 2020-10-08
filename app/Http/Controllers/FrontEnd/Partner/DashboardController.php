06ea333ef4b4e97386db203717ec2206f3e16b21
Elmer Torres <elmertalucod3@gmail.com>
2020-10-08 22:17:27 +0800

    done create dashboard for partner and admin

--- app/Http/Controllers/FrontEnd/Partner/DashboardController.php
+++ app/Http/Controllers/FrontEnd/Partner/DashboardController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers\FrontEnd\Partner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class QrCodeController extends Controller
+class DashboardController extends Controller
 {
     public function index(){
-        return view('front-end.partner.qr-code.index');
+        return view('front-end.partner.dashboard.index');
     }
 }
