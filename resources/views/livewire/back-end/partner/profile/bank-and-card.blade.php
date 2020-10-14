07804808a20dcbc8f71427803ec0afd1c98b82c0
Christian De Leon <cmdl.deleon02@gmail.com>
2020-10-14 16:53:37 +0800

    Created the list of banks in admin partner banks @ SYG-46

--- resources/views/livewire/back-end/partner/profile/bank-and-card.blade.php
+++ resources/views/livewire/back-end/partner/profile/bank-and-card.blade.php
@@ -11,7 +11,32 @@
             @if(!$banks)
                 <h4>No details yet</h4>
             @else
-            
+                @foreach($banks as $row)
+                <blockquote class="@if($row->is_default) quote-warning @else quote-secondary @endif">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-3">
+                            <div>
+                                <span class="fas fa-building-o"></span> <strong>{{$row->bank->name}}</strong> @if($row->is_default)<span class="badge badge-info">Default</span>@endif
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-3">
+                            <div title="Account Name">
+                                <span class="fas fa-user"></span> {{ucwords($row->account_name)}}
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-3">
+                            <div title="Account No.">
+                                <span class="fas fa-credit-card"></span> {{$row->account_no}}
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-3">
+                            <div>
+                                <button type="button" class="btn btn-sm btn-default" @if($row->is_default) disabled @else onclick="set_default('{{$row->key_token}}')" @endif>Set as Default</button>
+                            </div>
+                        </div>
+                    </div>
+                </blockquote>
+                @endforeach
             @endif
         </div>
     </div>
