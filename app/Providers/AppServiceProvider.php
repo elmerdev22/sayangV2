1b98fe0f259599063ecb03df9b69165451721555
elmer <mypoerfolio@gmail.com>
2020-11-05 22:55:40 +0800

    fix the error in migrating billing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,7 +10,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
         
         if(env('APP_ENV') === 'production'){
             URL::forceScheme('https');
