9fd83a2db387b7137c65c391b3aba00974ef56e4
elmer <mypoerfolio@gmail.com>
2020-11-10 21:27:48 +0800

    done AYG-169

--- app/Http/Livewire/FrontEnd/Partner/MyAccount/BanksAndCards/Listing.php
+++ app/Http/Livewire/FrontEnd/Partner/MyAccount/BanksAndCards/Listing.php
@@ -6,23 +6,85 @@ use Livewire\Component;
 use App\Model\UserAccount;
 use App\Model\Partner;
 use App\Model\PartnerBankAccount;
+use App\Model\Bank;
 use Utility;
-
+use DB;
 class Listing extends Component
 {
-    public $banks;
-    
+    public $my_banks, $banks_list;
+    public $bank, $account_name, $account_number, $key_token;
     public function mount(){
 		$account = Utility::auth_user_account();
 		$partner = Partner::with(['partner_bank_accounts', 'partner_bank_accounts.bank'])
 						->where('user_account_id', $account->id)
 						->first();
 		if($partner){
-			$this->banks = $partner->partner_bank_accounts()->get();
+			$this->my_banks = $partner->partner_bank_accounts()->get();
 		}
 	}
 
     public function render(){
+
+		$this->banks_list = $this->banks();
         return view('livewire.front-end.partner.my-account.banks-and-cards.listing');
+	}
+	
+	public function banks(){
+        return Bank::orderBy('name', 'asc')->get();
+	}
+	
+	public function edit($key_token){
+
+		$bank = PartnerBankAccount::where('key_token', $key_token)->first();
+
+		$this->bank           = $bank->bank_id;
+		$this->account_name   = $bank->account_name;
+		$this->account_number = $bank->account_no;
+		$this->key_token      = $key_token;
+
+	}
+
+    public function update(){
+
+        $rules = [
+			'bank'           => 'required',
+			'account_name'   => 'required',
+			'account_number' => 'required',
+        ];
+        
+		$this->validate($rules);
+		
+        $response = ['success' => false, 'message' => '',];
+        DB::beginTransaction();
+
+        try{
+			$bank               = PartnerBankAccount::where('key_token', $this->key_token)->first();
+			$bank->bank_id      = $this->bank;
+			$bank->account_name = $this->account_name;
+			$bank->account_no   = $this->account_number;
+
+            if($bank->save()){
+                $response['success'] = true;
+            }
+        }catch(\Exception $e){
+
+        }
+
+        if($response['success']){
+            DB::commit();
+            $this->emit('alert', [
+                'type'    => 'success',
+                'title'   => 'Successfully Updated',
+                'message' => 'Bank Details Successfully Updated!'
+            ]);
+            $this->mount();
+        }else{
+            DB::rollback();
+            $this->emit('alert', [
+                'type'    => 'error',
+                'title'   => 'Failed',
+                'message' => 'An error occured while updating information'
+            ]);
+        }
     }
 }
--- resources/views/livewire/front-end/partner/my-account/banks-and-cards/listing.blade.php
+++ resources/views/livewire/front-end/partner/my-account/banks-and-cards/listing.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @forelse($banks as $row)
+    @forelse($my_banks as $row)
         <blockquote class="@if($row->is_default) quote-warning @else quote-secondary @endif">
             <div class="row">
                 <div class="col-sm-6 col-md-3">
@@ -18,6 +18,9 @@
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-3">
+                    <button type="button" class="btn btn-sm btn-default" onclick="edit_modal('{{$row->key_token}}')">
+                        <span class="fas fa-edit"></span> Edit
+                    </button>
                     @if(!$row->is_default)
                         <button type="button" class="btn btn-sm btn-default">Set Default</button>
                     @else
@@ -30,4 +33,84 @@
     @empty
         <p class="text-center">You don't have bank account yet.</p>
     @endforelse
+
+    <!-- Modal -->
+    <div wire:ignore.self class="modal fade" id="modal-edit_bank" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Bank Details</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form wire:submit.prevent="update">
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label for="bank">Bank*</label>
+                                            <select class="form-control" id="bank" wire:model="bank">
+                                                <option value="">Select</option>
+                                                @foreach($banks_list as $row)
+                                                    <option value="{{$row->id}}">{{$row->name}}</option>
+                                                @endforeach
+                                            </select>
+                                            
+                                            @error('bank')
+                                                <span class="invalid-feedback" style="display: block;">
+                                                    <span>{{$message}}</span>
+                                                </span> 
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label for="account_name">Account Name*</label>
+                                            <input type="text" class="form-control text-capitalize" id="account_name" wire:model.lazy="account_name" placeholder="Enter Account Name">
+                                            
+                                            @error('account_name')
+                                                <span class="invalid-feedback" style="display: block;">
+                                                    <span>{{$message}}</span>
+                                                </span> 
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label for="account_number">Account Number*</label>
+                                            <input type="text" class="form-control" id="account_number" wire:model.lazy="account_number" placeholder="Enter Account Number">
+                                            
+                                            @error('account_number')
+                                                <span class="invalid-feedback" style="display: block;">
+                                                    <span>{{$message}}</span>
+                                                </span> 
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>  
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-warning">
+                            Save Changes <span wire:loading wire:target="update" class="fas fa-spinner fa-spin"></span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+@push('scripts')
+<script>
+    function edit_modal(key_token){
+        $('#modal-edit_bank').modal('show');
+        @this.call('edit', key_token)
+    }
+</script>
+@endpush
+
