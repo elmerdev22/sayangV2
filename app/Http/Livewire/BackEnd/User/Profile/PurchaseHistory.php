6707ac8760464c844fcd54c8c6a5200bb51e558c
Christian De Leon <cmdl.deleon02@gmail.com>
2020-11-16 10:03:28 +0800

    Added the list of purchase history of partner in their profile  @ SYG-183

--- app/Http/Livewire/BackEnd/User/Profile/PurchaseHistory.php
+++ app/Http/Livewire/BackEnd/User/Profile/PurchaseHistory.php
@@ -4,15 +4,64 @@ namespace App\Http\Livewire\BackEnd\User\Profile;
 
 use Livewire\Component;
 use App\Model\UserAccount;
+use Livewire\WithPagination;
+use App\Model\Order;
+use QueryUtility;
+use Utility;
 
 class PurchaseHistory extends Component
 {
+    use WithPagination;
+
 	public $account;
+    public $status, $search = '', $show_entries=10, $sort = [], $sort_type='desc';
 
 	public function mount($key_token){
 		$this->account = UserAccount::where('key_token', $key_token)->firstOrFail();
+        $this->sort    = ['orders.created_at'];
 	}
-    public function render(){
-        return view('livewire.back-end.user.profile.purchase-history');
+
+	public function data(){
+		$filter = [];
+		$filter['select'] = [
+			'orders.*', 
+			'user_accounts.first_name as user_account_first_name',
+			'user_accounts.last_name as user_account_last_name'
+		];
+		$filter['where']['billings.user_account_id'] = $this->account->id;
+
+		if($this->status != null){
+			$filter['where']['orders.status'] = $this->status;
+		}
+		
+		if($this->search){
+			$filter['or_where_like'] = $this->search;
+		}
+		
+		if($this->sort){
+			$sort_table = '';
+			foreach($this->sort as $key => $value){
+				$sort_table .= $value.' '.$this->sort_type.', ';
+			}
+			$sort_table = substr($sort_table, 0, -2);
+			$filter['order_by'] = $sort_table;
+		}
+
+		return QueryUtility::orders($filter)->paginate($this->show_entries);
     }
+    
+    public function updatingSearch(){
+		$this->resetPage();
+	}
+	
+    public function render(){
+		$data = $this->data();
+
+        return view('livewire.back-end.user.profile.purchase-history', compact('data'));
+	}
+
+	public function sort($sort){
+    	$this->sort_type   = $this->sort_type == 'asc' ? 'desc' : 'asc';
+    	return $this->sort = explode('|', $sort);
+	}
 }
