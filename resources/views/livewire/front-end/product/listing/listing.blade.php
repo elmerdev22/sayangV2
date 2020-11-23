adad51cc5bb58925e8130fcc9f8782a5f789b6b0
Christian De Leon <cmdl.deleon02@gmail.com>
2020-11-23 19:29:49 +0800

    Created the list view of sort in product list @ SYG-93

--- resources/views/livewire/front-end/product/listing/listing.blade.php
+++ resources/views/livewire/front-end/product/listing/listing.blade.php
@@ -21,64 +21,129 @@
 
     <div class="row">
         @forelse($data as $row)
-            <div class="@if($view_by == 'grid_view') col-lg-4 col-md-4 col-sm-6 col-6 @else col-12 @endif">
+            @if($view_by == 'grid_view')
+                <div class="col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="card mb-4 product-card">
+                        <div class="w-100 text-center">
+                            <div class="overflow-hidden position-relative">
+                                <img class="card-img-top sayang-card-img-listing img-preloader" src="{{$component->product_featured_photo($row->product_id, $row->partner_id)}}" alt="Card image cap">
+                                {{-- <span class="img-loader-span loader-span loader-quart"></span> --}}
+                            </div>
+                            <span class="ends-in">
+                                <div class="countdown text-white">
+                                    <span class="fas fa-clock"></span>
+                                    <span class="countdown">{{$component->datetime_format($row->date_end)}}</span>
+                                </div>
+                            </span>
+                            <div class="store-info p-1 mx-1 bg-transparent" style="margin-top: -30px; text-shadow: 0 0 3px black">
+                                <div class="row">
+                                    <div class="col-9 text-white text-left text-ellipsis">
+                                        {{ucfirst($row->partner_name)}}
+                                    </div>
+                                    <div class="col-3 text-right">
+                                        <span class="fas fa-star text-warning"></span> 
+                                        <span class="text-white">4.5</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product-info p-2">
+                                <div class="row">
+                                    <div class="col-8 font-weight-bold text-left text-ellipsis">
+                                        {{ucfirst($row->product_name)}}
+                                    </div>
+                                    <div class="col-4 text-right">
+                                        {{number_format($row->quantity)}} left
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row m-0 p-0">
+                                <div class="col-6 col-md-6 m-0 p-0">
+                                    <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'buy_now'])}}">
+                                        <button class="btn btn-sm btn-dark item-btn">
+                                            <span class="font-weight-bold">Buy Now</span><br>
+                                            <small class="text-white item-info">
+                                                PHP {{number_format($row->buy_now_price, 2)}} | 
+                                                {{Utility::price_percentage($row->regular_price, $row->buy_now_price)['discount_percent']}}% off
+                                            </small>
+                                        </button>
+                                    </a>
+                                </div>
+                                <div class="col-6 col-md-6 m-0 p-0">
+                                    <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'place_bid'])}}">
+                                        <button class="btn btn-sm btn-outline-warning text-dark item-btn">
+                                            <span class="font-weight-bold">Place Bid</span><br>
+                                            <small class="item-info">Bids: {{Utility::bid_details($row->id, 'count')}} | Top: {{Utility::bid_details($row->id, 'top')}}</small>
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @else
+            <div class="col-12">
                 <div class="card mb-4 product-card">
-                    <div class="w-100 text-center">
-                        <div class="overflow-hidden position-relative">
-                            <img class="card-img-top sayang-card-img-listing img-preloader" src="{{$component->product_featured_photo($row->product_id, $row->partner_id)}}" alt="Card image cap">
-                            {{-- <span class="img-loader-span loader-span loader-quart"></span> --}}
-                        </div>
-                        <span class="ends-in">
-                            <div class="countdown text-white">
-                                <span class="fas fa-clock"></span>
-                                <span class="countdown">{{$component->datetime_format($row->date_end)}}</span>
-                            </div>
-                        </span>
-                        <div class="store-info p-1 mx-1 bg-transparent" style="margin-top: -30px; text-shadow: 0 0 3px black">
-                            <div class="row">
-                                <div class="col-9 text-white text-left text-ellipsis">
-                                    {{ucfirst($row->partner_name)}}
-                                </div>
-                                <div class="col-3 text-right">
-                                    <span class="fas fa-star text-warning"></span> 
-                                    <span class="text-white">4.5</span>
-                                </div>
+                    <div class="row">
+                        <div class="col-sm-4 overflow-hidden product-card-img-list">
+                            <div class="overflow-hidden position-relative">
+                                <img class="card-img-top sayang-card-img-listing img-preloader" src="{{$component->product_featured_photo($row->product_id, $row->partner_id)}}" alt="Card image cap">
+                                {{-- <span class="img-loader-span loader-span loader-quart"></span> --}}
                             </div>
                         </div>
-                        <div class="product-info p-2">
-                            <div class="row">
-                                <div class="col-8 font-weight-bold text-left text-ellipsis">
-                                    {{ucfirst($row->product_name)}}
+                        <div class="col-sm-8 overflow-hidden">
+                            <div class="product-card-list-information">
+                                <div class="mb-3 product-card-countdown">
+                                    <span class="ends-in" style="position: relative !important;">
+                                        <div class="countdown text-white">
+                                            <span class="fas fa-clock"></span>
+                                            <span class="countdown">{{$component->datetime_format($row->date_end)}}</span>
+                                        </div>
+                                    </span>
                                 </div>
-                                <div class="col-4 text-right">
-                                    {{number_format($row->quantity)}} left
+                                <div class="mb-2">
+                                    <div class="store-info bg-transparent" style="text-shadow: 0 0 3px black">
+                                        <div class="text-left text-ellipsis">
+                                            {{ucfirst($row->partner_name)}} 
+                                            &nbsp;&nbsp;&nbsp;<span class="fas fa-star text-warning"></span> 
+                                            <span class="text-white">4.5</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-2">
+                                    <div class="product-info">
+                                        <div class="font-weight-bold text-left text-ellipsis">
+                                            {{ucfirst($row->product_name)}} ({{number_format($row->quantity)}} left)
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row m-0 p-0">
-                            <div class="col-md-6 m-0 p-0">
-                                <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'buy_now'])}}">
-                                    <button class="btn btn-sm btn-dark item-btn">
-                                        <span class="font-weight-bold">Buy Now</span><br>
-                                        <small class="text-white item-info">
-                                            PHP {{number_format($row->buy_now_price, 2)}} | 
-                                            {{Utility::price_percentage($row->regular_price, $row->buy_now_price)['discount_percent']}}% off
-                                        </small>
-                                    </button>
-                                </a>
-                            </div>
-                            <div class="col-md-6 m-0 p-0">
-                                <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'place_bid'])}}">
-                                    <button class="btn btn-sm btn-outline-warning text-dark item-btn">
-                                        <span class="font-weight-bold">Place Bid</span><br>
-                                        <small class="item-info">Bids: {{Utility::bid_details($row->id, 'count')}} | Top: {{Utility::bid_details($row->id, 'top')}}</small>
-                                    </button>
-                                </a>
+
+                            <div class="row product-card-btn-purchase">
+                                <div class="col-6 product-card-buy-now col-sm-5">
+                                    <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'buy_now'])}}">
+                                        <button class="btn btn-sm btn-dark item-btn">
+                                            <span class="font-weight-bold">Buy Now</span><br>
+                                            <small class="text-white item-info">
+                                                PHP {{number_format($row->buy_now_price, 2)}} | 
+                                                {{Utility::price_percentage($row->regular_price, $row->buy_now_price)['discount_percent']}}% off
+                                            </small>
+                                        </button>
+                                    </a>
+                                </div>
+                                <div class="col-6 product-card-place-bid col-sm-5">
+                                    <a href="{{route('front-end.product.information.redirect', ['slug' => $row->product_slug, 'key_token' => $row->key_token, 'type' => 'place_bid'])}}">
+                                        <button class="btn btn-sm btn-outline-warning text-dark item-btn">
+                                            <span class="font-weight-bold">Place Bid</span><br>
+                                            <small class="item-info">Bids: {{Utility::bid_details($row->id, 'count')}} | Top: {{Utility::bid_details($row->id, 'top')}}</small>
+                                        </button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            @endif
         @empty
             <div class="col-12">
                 <h4 class="text-center">No Item Found</h4>
