102e2a31ab4468d1d89110de3e08e5d2e5fe1c96
elmer <mypoerfolio@gmail.com>
2021-01-19 01:16:43 +0800

    add lazy loading

--- resources/views/front-end/layout.blade.php
+++ resources/views/front-end/layout.blade.php
@@ -70,89 +70,97 @@
     <!-- REQUIRED SCRIPTS -->
     <!-- jQuery -->
 
-  @livewireScripts
-  @if(env('APP_DEPLOY') == 'production')
-      <!-- Kindly removed once the packages need is working properly -->
-      <script src="{{asset('template/assets/plugins/jquery/jquery.min.js')}}"></script>
-      <!-- Bootstrap 4 -->
-      <script src="{{asset('template/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-      <!-- SweetAlert2 -->
-      <script src="{{ asset('template/assets/dist/js/sweetalert2.min.js') }}"></script>
-      <!-- Preloader -->
-      <script src="{{asset('template/assets/dist/js/preloader.js')}}"></script>
-      <!-- Admin lte -->
-      <script src="{{asset('template/assets/dist/js/adminlte.min.js')}}"></script>
-      <!-- Pusher JS -->
-      <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-      {{-- <script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script> --}}
-      <!-- Animate -->
-      {{-- <script src="{{asset('template/assets/dist/js/animate.js')}}"></script> --}}
-      <!-- Countdown JS -->
-      <script src="{{asset('template/assets/dist/js/countdown.js')}}"></script>
-      <!-- Custom JS -->
-      <script src="{{asset('template/assets/dist/js/custom.js')}}"></script>
-      <!-- end of to be removed packages -->
-  @else
-      <script src="{{ mix('js/app.js') }}"></script>
-  @endif
+    @livewireScripts
+    @if(env('APP_DEPLOY') == 'production')
+        <!-- Kindly removed once the packages need is working properly -->
+        <script src="{{asset('template/assets/plugins/jquery/jquery.min.js')}}"></script>
+        <!-- Bootstrap 4 -->
+        <script src="{{asset('template/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <!-- SweetAlert2 -->
+        <script src="{{ asset('template/assets/dist/js/sweetalert2.min.js') }}"></script>
+        <!-- Preloader -->
+        <script src="{{asset('template/assets/dist/js/preloader.js')}}"></script>
+        <!-- Admin lte -->
+        <script src="{{asset('template/assets/dist/js/adminlte.min.js')}}"></script>
+        <!-- Pusher JS -->
+        <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+        <!-- Lazy Loading -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js"></script>
+        <!-- Countdown JS -->
+        <script src="{{asset('template/assets/dist/js/countdown.js')}}"></script>
+        <!-- Custom JS -->
+        <script src="{{asset('template/assets/dist/js/custom.js')}}"></script>
+        <!-- end of to be removed packages -->
+    @else
+        <script src="{{ mix('js/app.js') }}"></script>
+    @endif
 
-  <script type="text/javascript">
-      // Enable pusher logging - don't include this in production
-      Pusher.logToConsole    = @if(env('APP_ENV') == 'local') true @else false @endif;
-          var pusher_key_app_key = '{{env('PUSHER_APP_KEY')}}';
-          var push_init          = new Pusher(pusher_key_app_key, {
-            cluster: 'ap1'
-      });
-
-    // badge-total-item-in-cart
-    window.livewire.on('initialize_cart_item_count', param => {
-        var total_item_in_cart = parseInt(param['total']);
-        $(document).find('.badge-total-item-in-cart').each(function () {
-            if(total_item_in_cart > 0){
-                $(this).html('<span class="badge badge-warning">'+total_item_in_cart+'</span>');
-            }else{
-                $(this).html('');
-            }
+    <script type="text/javascript">
+        $(function() {
+            $('.sayang-card-img-listing').lazy({
+                effect: "fadeIn",
+                effectTime: 1000,
+                autoDestroy: false,
+            });
         });
-    });
-
-    window.livewire.on('alert', param => {
-        var config = {
-            position  : 'center',
-        };
-
-        if('title' in param)
-            config['title'] = param['title'];
-        if('type' in param)
-            config['icon'] = param['type'];
-        if('message' in param)
-            config['html'] = param['message'];
-        if('showConfirmButton' in param)
-            config['showConfirmButton'] = param['showConfirmButton'];
-        if('timer' in param)
-            config['timer'] = param['timer'];
-
-        Swal.fire(config);
-    });
-
-    window.livewire.on('alert_link', param => {
-        Swal.fire({
-            position         : 'center',
-            icon             : param['type'],
-            html             : param['message'],
-            title            : param['title'],
-            showConfirmButton: true,
-            allowOutsideClick: false,
-        }).then((result) => {
-            if(result.value){
-                if('redirect' in param){
-                    window.location = param['redirect'];                       
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole    = @if(env('APP_ENV') == 'local') true @else false @endif;
+            var pusher_key_app_key = '{{env('PUSHER_APP_KEY')}}';
+            var push_init          = new Pusher(pusher_key_app_key, {
+                cluster: 'ap1'
+        });
+        
+        // badge-total-item-in-cart
+        window.livewire.on('initialize_cart_item_count', param => {
+            var total_item_in_cart = parseInt(param['total']);
+            $(document).find('.badge-total-item-in-cart').each(function () {
+                if(total_item_in_cart > 0){
+                    $(this).html('<span class="badge badge-warning">'+total_item_in_cart+'</span>');
                 }else{
-                    window.location.reload();                       
+                    $(this).html('');
                 }
-            }
+            });
         });
-    });
+
+        window.livewire.on('alert', param => {
+            var config = {
+                position  : 'center',
+            };
+
+            if('title' in param)
+                config['title'] = param['title'];
+            if('type' in param)
+                config['icon'] = param['type'];
+            if('message' in param)
+                config['html'] = param['message'];
+            if('showConfirmButton' in param)
+                config['showConfirmButton'] = param['showConfirmButton'];
+            if('timer' in param)
+                config['timer'] = param['timer'];
+
+            Swal.fire(config);
+        });
+
+        window.livewire.on('alert_link', param => {
+            Swal.fire({
+                position         : 'center',
+                icon             : param['type'],
+                html             : param['message'],
+                title            : param['title'],
+                showConfirmButton: true,
+                allowOutsideClick: false,
+            }).then((result) => {
+                if(result.value){
+                    if('redirect' in param){
+                        window.location = param['redirect'];                       
+                    }else{
+                        window.location.reload();                       
+                    }
+                }
+            });
+        });
+
     </script>
     @yield('js')
     @stack('scripts')
