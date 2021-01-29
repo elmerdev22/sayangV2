0593fcadfb2419230807fad40a96e6e1c181a679
elmer22 <elmertalucod3@gmail.com>
2021-01-29 15:16:42 +0800

    add redirect link in home category carousel

--- resources/views/livewire/front-end/home/category.blade.php
+++ resources/views/livewire/front-end/home/category.blade.php
@@ -5,10 +5,12 @@
             <div class="owl-carousel owl-theme">
                 @forelse($data as $catalog)
                 <div class="p-2">
-                    <img class="img-fluid img-circle shadow-sm" src="{{UploadUtility::category_photo($catalog->key_token)}}">
-                    <small class="d-none d-lg-block pt-2 text-center font-weight-bold">
-                        {{ucfirst($catalog->name)}}
-                    </small>
+                    <a href="{{route('front-end.product.list.index', ['category' => $catalog->slug])}}">
+                        <img class="img-fluid img-circle shadow-sm" src="{{UploadUtility::category_photo($catalog->key_token)}}">
+                        <small class="d-none d-lg-block pt-2 text-center font-weight-bold">
+                            {{ucfirst($catalog->name)}}
+                        </small>
+                    </a>
                 </div>
                 @empty
                 @endforelse
