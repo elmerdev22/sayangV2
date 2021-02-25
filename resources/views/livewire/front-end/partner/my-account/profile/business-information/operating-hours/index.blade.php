9ec8daed978df6e62c1ede44b9fd43eebcd9a871
elmer22 <elmertalucod3@gmail.com>
2021-02-25 12:59:35 +0800

    apply store hours in front page

--- resources/views/livewire/front-end/partner/my-account/profile/business-information/operating-hours/index.blade.php
+++ resources/views/livewire/front-end/partner/my-account/profile/business-information/operating-hours/index.blade.php
@@ -1,39 +1,41 @@
 <div>
-     <table class="table text-center table-cell-nowrap table-sm">
-        <thead>
-            <tr>
-                <th>Days</th>
-                <th>Open Time</th>
-                <th>Close Time</th>
-                <th>Status</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data->operating_hours as $row)
+    <div class="table-responsive">
+        <table class="table text-center table-cell-nowrap table-sm">
+            <thead>
                 <tr>
-                    <td>{{$days[$row->day]}}</td>
-                    <td>
-                        {{$row->open_time ? date('h:i A', strtotime($row->open_time)) : 'Not set'}}
-                    </td>
-                    <td>
-                        {{$row->close_time ? date('h:i A', strtotime($row->close_time)) : 'Not set'}}
-                    </td>
-                    <td>
-                        <div class="custom-control custom-switch">
-                            <input type="checkbox" class="custom-control-input" id="status{{$row->id}}" {{$row->status ? 'checked' : ''}} wire:change="status('{{$row->id}}')">
-                            <label class="custom-control-label" for="status{{$row->id}}"></label>
-                        </div>
-                    </td>
-                    <td>
-                        <button class="btn btn-sm btn-default" onclick="edit('{{$row->id}}','{{$days[$row->day]}}')">
-                            <span class="fas fa-edit"></span>
-                        </button>
-                    </td>
-                </tr>    
-            @endforeach
-        </tbody>
-    </table>
+                    <th>Days</th>
+                    <th>Open Time</th>
+                    <th>Close Time</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data->operating_hours as $row)
+                    <tr>
+                        <td>{{$days[$row->day]}}</td>
+                        <td>
+                            {{$row->open_time ? date('h:i A', strtotime($row->open_time)) : 'Not set'}}
+                        </td>
+                        <td>
+                            {{$row->close_time ? date('h:i A', strtotime($row->close_time)) : 'Not set'}}
+                        </td>
+                        <td>
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input" id="status{{$row->id}}" {{$row->status ? 'checked' : ''}} wire:change="status('{{$row->id}}')">
+                                <label class="custom-control-label" for="status{{$row->id}}"></label>
+                            </div>
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-default" onclick="edit('{{$row->id}}','{{$days[$row->day]}}')">
+                                <span class="fas fa-edit"></span>
+                            </button>
+                        </td>
+                    </tr>    
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     <!-- Modal Operating Hours -->
     <div wire:ignore.self class="modal fade" id="modal-operating_hours" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
