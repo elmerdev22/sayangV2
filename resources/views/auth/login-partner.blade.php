dadbcb453567f5f9d2d292673887e52814ede539
bob <elmertorres523@gmail.com>
2021-03-10 22:16:31 +0800

    remive the photo in register and login in partner

--- resources/views/auth/login-partner.blade.php
+++ resources/views/auth/login-partner.blade.php
@@ -2,28 +2,24 @@
 @section('title','Partner Login')
 @section('content')
 
-<section class="section-content padding-y">
+<!-- ========================= SECTION CONTENT ========================= -->
+<section class="section-content" style="min-height:84vh">
     <div class="container">
-        <!-- =========================  COMPONENT LOGIN PARTNER ========================= --> 
-        <div class="row">
-            <div class="col-md-7 d-none d-md-block d-lg-block" style="background: url('{{asset('images/default-photo/login-partner2.jpg')}}')  no-repeat center ">
-            </div>
-            <div class="col-md-5">
-                <div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
-                    <div class="card-body">
-                        <h4 class="card-title mb-4">Sign in as Partner</h4>
-                        <!-- ============================ COMPONENT LOGIN FORM  ================================= -->
-                        @livewire('auth.login-partner')
-                        <!-- ============================ COMPONENT LOGIN  FORM END.// ================================= -->
-                    </div> <!-- card-body.// -->
-                </div> <!-- card .// -->
-                <p class="text-center mt-4">Not Yet a Partner? <a href="{{route('partner.register')}}">Be a partner now!</a></p>
-                <br><br>
-            </div>
-        </div> <!-- row.// -->
-        <!-- =========================  COMPONENT LOGIN PARTNER.// ========================= --> 
-    </div> <!-- container .//  -->
+        <!-- ============================ COMPONENT LOGIN   ================================= -->
+        <div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
+            <div class="card-body">
+                <h4 class="card-title mb-4">Sign in as Partner</h4>
+                <!-- ============================ COMPONENT LOGIN FORM  ================================= -->
+                @livewire('auth.login-partner')
+                <!-- ============================ COMPONENT LOGIN  FORM END.// ================================= -->
+            </div> <!-- card-body.// -->
+        </div> <!-- card .// -->
+        <p class="text-center mt-4">Not Yet a Partner? <a href="{{route('partner.register')}}">Be a partner now!</a></p>
+        <br><br>
+        <!-- ============================ COMPONENT LOGIN  END.// ================================= -->
+    </div>
 </section>
+<!-- ========================= SECTION CONTENT END// ========================= -->
 @endsection
 @section('js')
     @if(session('success_change_password'))
--- resources/views/auth/register-partner.blade.php
+++ resources/views/auth/register-partner.blade.php
@@ -4,32 +4,20 @@
 <!-- ========================= SECTION CONTENT ========================= -->
 <section class="section-content padding-y">
     <div class="container">
-        <div class="row">
-            <div class="col-md-6 d-none d-md-block d-lg-block" style="background: url('{{asset('images/default-photo/login-partner2.jpg')}}')  no-repeat center ">
-                <div class="jumbotron bg-transparent ">
-                    <h1 class="jumbotron-heading display-3">
-                        <b>Grow with us.</b>
-                    </h1>
-                    <p class="lead">Zero Waste. Maximum Profits.</p>
-                  </div>
-            </div>
-            <div class="col-md-6">
-                <!-- ============================ COMPONENT REGISTER   ================================= -->
-                <div class="card mx-auto" style="max-width:520px; margin-top:40px;">
-                    <article class="card-body">
-                        <header class="mb-4"><h4 class="card-title">Be a Partner</h4></header>
-                        
-                        <!-- ============================ COMPONENT REGISTER FORM  ================================= -->
-                        @livewire('auth.register-partner')
-                        <!-- ============================ COMPONENT REGISTER  FORM END.// ================================= -->
-        
-                    </article><!-- card-body.// -->
-                </div> <!-- card .// -->
-                <p class="text-center mt-4">Have an account? <a href="{{route('partner.login')}}">Sign In</a></p>
-                <br><br>
-                <!-- ============================ COMPONENT REGISTER  END.// ================================= -->
-            </div>
-        </div> <!-- row.// -->
+        <!-- ============================ COMPONENT REGISTER   ================================= -->
+        <div class="card mx-auto" style="max-width:520px; margin-top:40px;">
+            <article class="card-body">
+                <header class="mb-4"><h4 class="card-title">Be a Partner</h4></header>
+                
+                <!-- ============================ COMPONENT REGISTER FORM  ================================= -->
+                @livewire('auth.register-partner')
+                <!-- ============================ COMPONENT REGISTER  FORM END.// ================================= -->
+
+            </article><!-- card-body.// -->
+        </div> <!-- card .// -->
+        <p class="text-center mt-4">Have an account? <a href="{{route('partner.login')}}">Sign In</a></p>
+        <br><br>
+        <!-- ============================ COMPONENT REGISTER  END.// ================================= -->
     </div>
 </section>
 <!-- ========================= SECTION CONTENT END// ========================= -->  
