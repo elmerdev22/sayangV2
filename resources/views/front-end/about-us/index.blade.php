451b8024994e057b8541a503f27fbe9fa4c2bd73
bob <elmertorres523@gmail.com>
2021-05-15 19:36:16 +0800

    changes in about us

--- resources/views/front-end/about-us/index.blade.php
+++ resources/views/front-end/about-us/index.blade.php
@@ -19,13 +19,16 @@
             <div class="col-md-6">
                 <h1 class="text-primary display-3 mb-5 font-weight-bolder">We are <br> dreamers</h1>
                 <p>
-                    We dream of a world where no perfectly good, consumable, or usable product goes to waste.
+                    We dream of a world where no perfectly good, consumable, or usable product goes to waste. 
                 </p>
                 <p>
-                    Our spending habits and standards inadvertently lead to waste. Groceries adhere to strict aesthetic guidelines for their fresh produce where a fruit with a minuscule brown spot will be thrown away instead of being sold. Traditional ecommerce sites require very long remaining shelf life, leading to the disposal of perfectly good products even if it is just a few days past the cut-off.
+                    Did you know that 70% of terrestrial biodiversity loss and 69% of global freshwater use is attributed to food consumption and production? Evidently, food waste is one of the biggest threat to nature today.
+                </p>
+                <p>
+                    Our spending habits and standards inadvertently lead to waste. Businesses such as supermarkets and grocery chains opt not to sell “unaesthetic” items, overstocks, or items near the expiration date. Likewise, these products are usually ignored by the market, who would opt to go for “perfectly-looking” items. Thus, for instance, a fruit with a minuscule brown spot or a canned good that is still months away from expiry, will just be thrown away instead of being sold. Traditional ecommerce sites require long remaining shelf life, leading to the disposal- turning perfectly consumable products to unnecessary waste. 
                 </p>
                 <p class="mb-3">
-                    We believe that by creating a platform for manufacturers and users alike will help reduce and eventually get rid of this unnecessary waste.
+                    We believe that by creating a platform that will drive sustainable production and consumption for both businesses and consumers, we can reduce and eventually get rid of unnecessary waste. With innovation and inspiration, it is possible to prove that there is really no excuse nor place for unnecessary waste here in the Philippines.
                 </p>
             </div> 
             <div class="col-md-6">
@@ -94,10 +97,10 @@
             <div class="col-md-6">
                 <h1 class="text-primary display-4 mb-5 font-weight-bolder">About Us</h1>
                 <p>
-                    Our team is Our purpose is to be part of solutions that tackle our world's biggest problems, and what better way to drive this purpose than helping eliminate waste and improve lives and businesses in the process?
+                    Our purpose is to be part of solutions that tackle our world's biggest problems, and what better way to drive this purpose than helping reduce waste while improving businesses’ bottom line in the process?
                 </p>
                 <p>
-                    Our team is composed of young, passionate professionals turned entrepreneurs. As each one of us were previously based in Metro Manila, we witnessed first hand how wastes affect people, businesses, and the planet.
+                    Our team is composed of young, passionate professionals-turned-entrepreneurs. As each one of us were previously based in Metro Manila, we witnessed first-hand how wastes affect people, businesses, and the planet.
                 </p>
             </div> 
         </div>                    
@@ -112,13 +115,13 @@
             <div class="col-md-6">
                 <h1 class="text-primary display-4 mb-5 font-weight-bolder">Our Story</h1>
                 <p>
-                    Our idea was borne out of necessity. While the pandemic raged on last 2020, we witnessed people and businesses struggling to make ends meet.
+                    Our idea was borne out of necessity. While the pandemic raged on last 2020, we witnessed people and businesses struggling to make ends meet. Both large and small businesses alike faced with figuring out what to do with their slow-moving and near-expiry inventories. 
                 </p>
                 <p>
-                    We witnessed both large and small businesses alike come up with different schemes to sell their excess inventory. Top fast food companies tried going retail, FMCGs turned to ecommerce promos, and small businesses tried their luck with online selling. Despite these huge efforts, we still saw the devastating financial impact of the pandemic as their products remain unsold.
+                    Top fast-food companies tried going retail, FMCGs turned to ecommerce promos, and small businesses tried their luck with online selling. Despite these huge efforts, we still saw the devastating financial impact of the pandemic as their products remain unsold and eventually become a loss upon disposal. Businesses must find new ways to salvage not only the product but the potential revenue.
                 </p>
                 <p>
-                    On the other hand, people were lining up for hours to receive their much needed relief, some of whom have not earned anything for days due to the lockdowns.
+                    On the other hand, people were lining up for hours to receive their much-needed relief, some of whom have not earned anything for days due to the lockdowns- looking for the most affordable goods available in the market. 
                 </p>
                 <p class="mb-3">
                     We find it unjust that while products expire in our partners' warehouses, the most vulnerable cannot get their hands on much needed essentials due to lack of cash.
@@ -142,7 +145,7 @@
             <div class="col-md-6">
                 <h1 class="text-primary display-4 mb-5 font-weight-bolder">NOTHING GOES TO WASTE</h1>
                 <p>
-                    We believe that by creating a platform for these products, we can help eliminate waste, help businesses, and help people all at the same time.
+                    We believe that by creating a platform for these products, we can help eliminate waste, help businesses, and help people all at the same time. With Sayang.ph, we can help to drive sustainable development here in the Philippines.
                 </p>
             </div> 
         </div>                    
